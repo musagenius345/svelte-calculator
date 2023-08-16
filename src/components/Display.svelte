dc38a00c67f32a84acc38ee9273ed5415561c1ff
musagenius345 <musabadru@gmail.com>
2023-08-16 09:06:32 +0300

    mathjs calibration

--- src/components/Display.svelte
+++ src/components/Display.svelte
@@ -1,12 +1,11 @@
 <script>
-  import { evaluate } from 'mathjs'
+  import { evaluate, fraction } from 'mathjs'
   import Button from "./Button.svelte"
 
 
   $: total = 0
   $: numberInput = ''
   $: cursorPosition = 0
-  let autocomplete = false
 
 
   function addToInput(value) {
@@ -22,32 +21,13 @@
   }
 
 
-  function moveCursorLeft() {
-    if (cursorPosition > 0) {
-      cursorPosition--;
-    }
-  }
-
-  function moveCursorRight() {
-    if (cursorPosition < inputValue.length) {
-      cursorPosition++;
-    }
-  }
-
-
-
   //FIXIT
   const equal = () => {
-    numberInput = total
-  }
-  const answer = () => {
-    numberInput = total
+    numberInput = total.toString()
   }
 
-
-  //TODO Make it work
-  const fractional = () => {
-    numberInput = fraction(numberInput)
+  const fractional= () => {
+  return fraction(numberInput)
   }
 
 
@@ -62,7 +42,10 @@
   }
 
 
-
+function handleInput(event) {
+  cursorPosition = event.target.selectionStart;
+  inputManual(event);
+}
 
 
 
@@ -87,7 +70,7 @@
 
 <div class="wrapper">
   <div class="screen-display">
-    <input id="arithmetic" type="text" autocomplete=off on:input={inputManual} bind:value={numberInput} width="100" bind:selectionStart={cursorPosition} bind:selectionEnd={cursorPosition} />
+    <input id="arithmetic" type="text" autocomplete=off on:input={inputManual} bind:value={numberInput} on:input={handleInput}/>
     <output name="result" for="arithmetic" placeholder="Results">{total}</output>
   </div>
 </div>
@@ -99,23 +82,23 @@
       <circle cx="12" cy="12" r="3" />
       <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
     </svg></Button>
-  <Button gray="true" on:click={()=> addToInput('!')}>FACT</Button>
-  <Button gray="true" on:click={()=> addToInput('^')}>a<sup style="margin-inline-start: .2em">b</sup></Button>
+  <Button gray on:click={()=> addToInput('!')}>FACT</Button>
+  <Button gray on:click={()=> addToInput('^')}>a<sup style="margin-inline-start: .2em">b</sup></Button>
   <Button on:click={()=> addToInput('7')}>7</Button>
   <Button on:click={()=> addToInput('8')}>8</Button>
   <Button on:click={()=> addToInput('9')}>9</Button>
   <Button on:click={()=> addToInput('/')} >&divide;</Button>
-  <Button orange="true" on:click={del}>
+  <Button orange on:click={del}>
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z" />
       <line x1="18" y1="9" x2="12" y2="15" />
       <line x1="12" y1="9" x2="18" y2="15" />
     </svg>
   </Button>
-  <Button dark="false" orange="true" on:click={()=>clearInput()}>AC</Button>
-  <Button gray="true">M&plus;</Button>
-  <Button gray="true">M&minus;</Button>
-  <Button gray="true" on:click={fractional}><svg id="ei4kL3yKbWI1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" style="background-color:transparent; transform: translateY(-8px);">
+  <Button orange on:click={()=>clearInput()}>AC</Button>
+  <Button gray>M&plus;</Button>
+  <Button gray>M&minus;</Button>
+  <Button gray on:click={fractional}><svg id="ei4kL3yKbWI1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" style="background-color:transparent; transform: translateY(-8px);">
       <rect width="30" height="30" rx="0" ry="0" transform="translate(136.15 105)" fill="#fff" stroke-width="0" />
       <rect width="30" height="30" rx="0" ry="0" transform="translate(136.149972 154.448797)" fill="#fff" stroke-width="0" />
       <line x1="-15" y1="0" x2="35" y2="0" transform="translate(141 144)" fill="#fff" stroke="#fff" stroke-width="3" />
@@ -124,25 +107,25 @@
   <Button on:click={()=> addToInput('5')}>5</Button>
   <Button on:click={()=> addToInput('6')}>6</Button>
   <Button on:click={()=> addToInput('*')}>&times;</Button>
-  <Button gray="true">&#8592;</Button>
-  <Button gray="true">&#8594;</Button>
-  <Button gray="true">MR</Button>
-  <Button gray="true">MC</Button>
-  <Button gray="true" on:click={()=> addToInput('√')}>&radic;</Button>
+  <Button gray>&#8592;</Button>
+  <Button gray>&#8594;</Button>
+  <Button gray>MR</Button>
+  <Button gray>MC</Button>
+  <Button gray on:click={()=> addToInput('√')}>&radic;</Button>
   <Button on:click={()=> addToInput('1')}>1</Button>
   <Button on:click={()=> addToInput('2')}>2</Button>
   <Button on:click={()=> addToInput('3')}>3</Button>
   <Button on:click={()=> addToInput(' - ')}>&minus;</Button>
-  <Button gray="true" on:click={()=> addToInput('E')}>Exp</Button>
-  <Button gray="true" on:click={()=> answer()}>Ans</Button>
-  <Button gray="true" on:click={()=> fractional()}>S&lrhar;D</Button>
-  <Button gray="true" on:click={()=> addToInput('(')}>&lpar;</Button>
-  <Button gray="true" on:click={()=> addToInput(')')}>&rpar;</Button>
+  <Button gray on:click={()=> addToInput('E')}>Exp</Button>
+  <Button gray on:click={()=> equal()}>Ans</Button>
+  <Button gray on:click={()=> fractional()}>S&lrhar;D</Button>
+  <Button gray on:click={()=> addToInput('(')}>&lpar;</Button>
+  <Button gray on:click={()=> addToInput(')')}>&rpar;</Button>
   <Button on:click={()=> addToInput('%')}>&percnt;</Button>
   <Button on:click={()=> addToInput('0')}>0</Button>
   <Button on:click={()=> addToInput('.')}>&sdot;</Button>
   <Button on:click={()=> addToInput('+')}>&plus;</Button>
-  <Button gray="true" largeBtn="true" on:click={()=> equal()}>&equals;</Button>
+  <Button gray largeBtn on:click={()=> equal()}>&equals;</Button>
 </div>
 <style>
   .numpad {
@@ -188,7 +171,7 @@
     gap: 1.2rem;
   }
 
-  .wrapper {}
+  /* .wrapper {} */
 
   #arithmetic {
     width: calc(93% - 1.8rem);
